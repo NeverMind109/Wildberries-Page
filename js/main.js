2f18755cf4fadd14e40cc266aaf98bd2565e4e0e
NeverMind109 <vladgladkih123@gmail.com>
2021-11-30 15:33:58 +0300

    Created: Added Swiper Slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    speed: 600,
+ 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  });
